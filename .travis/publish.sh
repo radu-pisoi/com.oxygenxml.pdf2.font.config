675abd5db0242fdbdcdc8d61eddc6888c0d295af
radu_pisoi <radupisoi23@gmail>
2016-12-20 19:24:36 +0200

    Debug transformation

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -36,10 +36,14 @@ sh dita-ot-2.4/bin/dita --install
 echo "====================================="
 echo "Transform to PDF the sample DITA map"
 echo "====================================="
-sh dita-ot-2.4/bin/dita -i "DITA sample/font-configuration.ditamap" -f pdf2 -output=publishing -v  -Dorg.dita.pdf2.i18n.enabled=false 
+sh dita-ot-2.4/bin/dita -i "DITA sample/font-configuration.ditamap" -f pdf2 -output=publishing -v  -Dorg.dita.pdf2.i18n.enabled=false -Dclean.temp=no 
 
 # cp gh-pages/index.html publishing/index.html
 echo "====================================="
 echo "List output file"
 echo "====================================="
+
+ls -la sample
+ls -la sample/temp
+
 ls -la publishing
